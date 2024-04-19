bb175f1690031f26f903506da90779b9c04e180f
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-19 12:30:57 +0200

    Add ActivityBaseModel class and export it in index.ts

---
+++ src/classes/Activity.ts
@@ -0,0 +1,6 @@
+export interface ActivityBaseModelProps {
+}
+
+export default class ActivityBaseModel {
+    constructor(id: string, props: ActivityBaseModelProps) { }
+}
---
+++ src/classes/index.ts
@@ -0,0 +1,1 @@
+export { default as ActivityBaseModel } from './Activity';
