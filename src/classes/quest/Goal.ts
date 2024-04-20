bc1d307a4721f58fe85fe5b1619eda6981b4f685
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-20 12:44:53 +0200

    Refactor GoalBaseModel class to update properties and add new ones

--- src/classes/quest/Goal.ts
+++ src/classes/quest/Goal.ts
@@ -1,6 +1,38 @@
-export interface GoalBaseModelProps {
+export interface GoalProps {
+    description: string
+    need?: number
+    have?: number
 }
 
-export default class GoalBaseModel {
-    constructor(id: string, props: GoalBaseModelProps) { }
+export interface IGoal {
+    description: string
+    need: number
+    have: number
+}
+
+export default class Goal implements IGoal {
+    constructor(props: GoalProps) {
+        this._description = props.description
+        this.need = props.need || 1
+        this.have = props.have || 0
+    }
+    private _description: string
+    get description(): string {
+        return this._description
+    }
+    need: number
+    have: number
+    get isComplete(): boolean {
+        return this.have >= this.need
+    }
+    setComplete(): void {
+        this.have = this.need
+    }
+    get export(): IGoal {
+        return {
+            description: this.description,
+            need: this.need,
+            have: this.have
+        }
+    }
 }
