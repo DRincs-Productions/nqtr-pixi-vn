eaec31f095d63a1da7a8c76b963bb15644124f7e
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-20 16:22:56 +0200

    Refactor QuestBaseModel class to update properties and add new ones

--- src/classes/quest/Quest.ts
+++ src/classes/quest/Quest.ts
@@ -1,12 +1,53 @@
 import { StoredClassModel } from "@drincs/pixi-vn";
+import StageBaseModel from "./Stage";
 
 const QUEST_PREFIX = "__NQTR-Quest__"
 
 export interface QuestBaseModelProps {
+    name?: string
+    description?: string
+    icon?: string
+    descriptionImage?: string
+    isInDevelopment?: boolean
 }
 
-export default class QuestBaseModel extends StoredClassModel {
-    constructor(id: string, props: QuestBaseModelProps) {
+export default class QuestBaseModel<TStage extends StageBaseModel = StageBaseModel> extends StoredClassModel {
+    constructor(id: string, stages: TStage[], props: QuestBaseModelProps) {
         super(QUEST_PREFIX + id)
+        this._stages = stages
+        this._name = props.name || ""
+        this._description = props.description || ""
+        this._icon = props.icon || ""
+        this._descriptionImage = props.descriptionImage || ""
+        this._isInDevelopment = props.isInDevelopment || false
+    }
+    private _stages: TStage[]
+    get stages(): TStage[] {
+        return this._stages
+    }
+
+    private _name: string
+    get name(): string {
+        return this._name
+    }
+
+    private _description: string
+    get description(): string {
+        return this._description
+    }
+
+    private _icon: string
+    get icon(): string {
+        return this._icon
+    }
+
+    private _descriptionImage: string
+    get descriptionImage(): string {
+        return this._descriptionImage
+    }
+
+    private _isInDevelopment: boolean
+    get isInDevelopment(): boolean {
+        return this._isInDevelopment
     }
 }
