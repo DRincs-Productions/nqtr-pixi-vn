ef858cb27c0494f2de9ed9d772878080adaee72c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-20 12:45:18 +0200

    Refactor GoalBaseModel class to update import statement for Goal

--- src/classes/quest/index.ts
+++ src/classes/quest/index.ts
@@ -1,4 +1,4 @@
-export { default as GoalBaseModel, GoalBaseModelProps } from './Goal';
+export { default as Goal, GoalProps } from './Goal';
 export { default as QuestBaseModel, QuestBaseModelProps } from './Quest';
 export { default as StageBaseModel, StageBaseModelProps } from './Stage';
 
