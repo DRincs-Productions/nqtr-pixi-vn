1ba42b65ef77866cbf9af1078bb7a0f7ffde93f8
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-24 16:38:22 +0200

    Refactor NavigationFunctions.ts to include setCurrentRoom and getCurrentRoom functions

---
+++ src/decorators/RoomDecorator.ts
@@ -0,0 +1,29 @@
+import { RoomBaseModel } from "../classes/navigation"
+
+export const registeredRooms: { [id: string]: RoomBaseModel } = {}
+
+export function saveRoom<TRoom extends RoomBaseModel = RoomBaseModel>(room: TRoom | TRoom[]) {
+    if (Array.isArray(room)) {
+        room.forEach(c => saveRoom(c))
+        return
+    }
+    if (registeredRooms[room.id]) {
+        console.warn(`[NQTR] Room id ${room.id} already exists, it will be overwritten`)
+    }
+    registeredRooms[room.id] = room
+}
+
+export function getRoomById<TRoom extends RoomBaseModel = RoomBaseModel>(id: string): TRoom | undefined {
+    try {
+        let room = registeredRooms[id]
+        if (!room) {
+            console.error(`[NQTR] Room ${id} not found`)
+            return
+        }
+        return room as TRoom
+    }
+    catch (e) {
+        console.error(`[NQTR] Error while getting Room ${id}`, e)
+        return
+    }
+}
--- src/functions/NavigationFunctions.ts
+++ src/functions/NavigationFunctions.ts
@@ -1,1 +1,28 @@
+import { GameStorageManager } from "@drincs/pixi-vn";
+import { RoomBaseModel } from "../classes/navigation";
+import { getRoomById } from "../decorators/RoomDecorator";
+
 const CURRENT_ROOM_MEMORY_KEY = '___nqtr_current_room_memory___';
+
+export function setCurrentRoom<TRoom extends RoomBaseModel = RoomBaseModel>(room: TRoom) {
+    let roomRegistrated = getRoomById(room.id);
+    if (!roomRegistrated) {
+        console.error(`[NQTR] The room ${room.id} is not registered, so it can't be set as current room`);
+        return;
+    }
+    localStorage.setItem(CURRENT_ROOM_MEMORY_KEY, room.id);
+}
+
+export function getCurrentRoom<TRoom extends RoomBaseModel = RoomBaseModel>(): TRoom | undefined {
+    let roomId = GameStorageManager.getVariable<string>(CURRENT_ROOM_MEMORY_KEY);
+    if (!roomId) {
+        console.error(`[NQTR] The current room has not yet been set`);
+        return
+    }
+    let room = getRoomById<TRoom>(roomId);
+    if (!room) {
+        console.error(`[NQTR] Current room ${roomId} not found`);
+        return
+    }
+    return room
+}
