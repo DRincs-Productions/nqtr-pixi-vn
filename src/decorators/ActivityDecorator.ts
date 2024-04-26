6089714975f305c527af6db6287283fa05b9dbba
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-26 13:14:42 +0200

    Refactor ActivityDecorator.ts to use ActivityModel instead of ActivitySynchronizedModel

--- src/decorators/ActivityDecorator.ts
+++ src/decorators/ActivityDecorator.ts
@@ -1,21 +1,21 @@
 import { ActivityModel } from "../classes"
 
-export const registeredActivitys: { [id: string]: ActivityModel } = {}
+export const registeredActivities: { [id: string]: ActivityModel } = {}
 
 export function saveActivity(activity: ActivityModel | ActivityModel[]) {
     if (Array.isArray(activity)) {
         activity.forEach(c => saveActivity(c))
         return
     }
-    if (registeredActivitys[activity.id]) {
+    if (registeredActivities[activity.id]) {
         console.warn(`[NQTR] Activity id ${activity.id} already exists, it will be overwritten`)
     }
-    registeredActivitys[activity.id] = activity
+    registeredActivities[activity.id] = activity
 }
 
 export function getActivityById(id: string): ActivityModel | undefined {
     try {
-        let activity = registeredActivitys[id]
+        let activity = registeredActivities[id]
         if (!activity) {
             console.error(`[NQTR] Activity ${id} not found`)
             return
