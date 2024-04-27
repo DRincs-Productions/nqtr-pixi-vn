5c03b107a32119f10666c76f84eda504b6d9afec
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-04-27 13:11:19 +0200

    Refactor getCommitmentByCharacter function in RoutineFunctions.ts

--- src/functions/RoutineFunctions.ts
+++ src/functions/RoutineFunctions.ts
@@ -1,4 +1,4 @@
-import { CharacterBaseModel, getAllCharacters } from "@drincs/pixi-vn";
+import { CharacterBaseModel } from "@drincs/pixi-vn";
 import CommitmentBaseModel from "../classes/Commitment";
 import { getFixedCommitments, getTemporaryCommitments } from "../decorators";
 import { registeredCommitments } from "../decorators/CommitmentDecorator";
@@ -11,35 +11,45 @@ export function clearExpiredRoutine() {
     })
 }
 
-export function getCurrentCommitments<TCommitment extends CommitmentBaseModel = CommitmentBaseModel>(): TCommitment[] {
-    let commitments: TCommitment[] = []
-    getAllCharacters().forEach(character => {
-        const commitment = getCommitmentByCharacter<TCommitment>(character)
-        if (commitment) {
-            commitments.push(commitment)
-        }
-    })
-    return commitments
-}
-
 // TODO: In the case of group commitments, it does not check whether the other characters are available for this commitment
-export function getCommitmentByCharacter<TCommitment extends CommitmentBaseModel = CommitmentBaseModel, TCharacter extends CharacterBaseModel = CharacterBaseModel>(character: TCharacter): TCommitment | undefined {
+export function getCurrentCommitments<TCommitment extends CommitmentBaseModel = CommitmentBaseModel>(): TCommitment[] {
+    let character_commitments: { [character: string]: TCommitment } = {}
+    let oltherCommitments: TCommitment[] = []
     getTemporaryCommitments<TCommitment>().reverse().forEach(c => {
         if (!c.hidden) {
-            c.characters.forEach(ch => {
-                if (ch.id === character.id) {
-                    return c
-                }
-            })
+            if (c.characters.length > 1) {
+                c.characters.forEach(ch => {
+                    if (!character_commitments[ch.id]) {
+                        character_commitments[ch.id] = c
+                    }
+                })
+            }
+            else {
+                oltherCommitments.push(c)
+            }
         }
     })
     getFixedCommitments<TCommitment>().reverse().forEach(c => {
         if (!c.hidden) {
-            c.characters.forEach(ch => {
-                if (ch.id === character.id) {
-                    return c
-                }
-            })
+            if (c.characters.length > 1) {
+                c.characters.forEach(ch => {
+                    if (!character_commitments[ch.id]) {
+                        character_commitments[ch.id] = c
+                    }
+                })
+            }
+            else {
+                oltherCommitments.push(c)
+            }
+        }
+    })
+    return Object.values(character_commitments).concat(oltherCommitments)
+}
+
+export function getCommitmentByCharacter<TCommitment extends CommitmentBaseModel = CommitmentBaseModel, TCharacter extends CharacterBaseModel = CharacterBaseModel>(character: TCharacter): TCommitment | undefined {
+    getCurrentCommitments().forEach(c => {
+        if (c.characters.map(ch => ch.id).includes(character.id)) {
+            return c
         }
     })
     return undefined
