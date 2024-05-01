356d28c35563980421d89a468fe126daa0c12157
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-05-01 14:41:01 +0200

    Update TimeManager settings initialization method to include time slots and weekend start day

--- src/managers/TimeManager.ts
+++ src/managers/TimeManager.ts
@@ -18,13 +18,31 @@ export interface TimeSettings {
     defaultTimeSpent?: number
     /**
      * Time slots (default: [])
+     * @example
+     * ```ts
+     * [
+     *   { name: 'Morning', startHour: 5 },
+     *   { name: 'Afternoon', startHour: 12 },
+     *   { name: 'Evening', startHour: 18 },
+     *   { name: 'Night', startHour: 22 }
+     * ]
      */
     timeSlots?: ITimeStlot[]
     /**
      * Week length (default: 7)
      */
     weekLength?: number
+    /**
+     * Weekend start day. For example, if the real life weekend starts on Saturday, then the value should be 6 (default: weekLength - 1)
+     */
     weekendStartDay?: number
+    /**
+     * Week days names (default: [])
+     * @example
+     * ```ts
+     * ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+     * ```
+     */
     weekDaysNames?: string[]
 }
 
