c163fa3c9f179a09e01a55bb50c75d34e20ef985
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-02 21:47:45 +0200

    refactor: Update index.ts to export ActivityProps interface

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './classes/quest';
 export * from './decorators';
 export * from './enums';
 export * from './functions';
+export * from './interface';
 export * from './managers';
 export * from './types';
 
