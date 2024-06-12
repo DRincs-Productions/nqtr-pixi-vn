6ae0ec47974753d5064c62fb104d1dfb18064bec
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2024-06-12 16:28:33 +0200

    refactor: Add QuestDecorator to index.ts

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,4 +1,5 @@
 export { getActivityById, saveActivity } from './ActivityDecorator';
 export { addCommitment, getCommitmentById, saveCommitment, setFixedRoutine } from './CommitmentDecorator';
+export { getQuestById, saveQuest } from './QuestDecorator';
 export { getAllLocations, getAllMaps, getRoomById, saveRoom } from './RoomDecorator';
 
