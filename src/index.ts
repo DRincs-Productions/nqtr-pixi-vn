6ca8ab676a36ee465a6d1195c7901a47e8e87fed
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-15 16:40:56 +0100

    feat: restructure exports in index.ts and update TypeScript configuration

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,20 @@ export * from './interface';
 export * from './managers';
 export * from './types';
 
+import * as classes from './classes';
+import * as decorators from './decorators';
+import * as functions from './functions';
+import * as interfaceNqtr from './interface';
+import * as managers from './managers';
+import * as types from './types';
+
+const nqtr = {
+    ...classes,
+    ...decorators,
+    ...functions,
+    ...interfaceNqtr,
+    ...managers,
+    ...types,
+};
+export default nqtr
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/**/*@(ts|tsx)"],
+    entry: ["src/**/*.ts"],
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     treeshake: true,
     splitting: false,
-    sourcemap: true,
+    sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
     clean: true,
+    minify: true,
+    bundle: true,
+    skipNodeModulesBundle: false, // Skip bundling of node_modules
+    entryPoints: ['src/index.ts'],
 });
