458cbddf872bee915c7f5af9baa7c19c90364db5
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-17 23:36:51 +0100

    add MapInterface and export it from the override index

--- src/override/index.ts
+++ src/override/index.ts
@@ -1,5 +1,6 @@
 export { default as GraphicItemInterface } from './GraphicItemInterface';
 export { default as GraphicItemType } from './GraphicItemType';
+export { default as MapInterface } from './MapInterface';
 export { default as NeighboringMapsInterface } from './NeighboringMapsInterface';
 export { default as OnRenderGraphicItemProps } from './OnRenderGraphicItemProps';
 export { default as OnRunProps } from './OnRunProps';
