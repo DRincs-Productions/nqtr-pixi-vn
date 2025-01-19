ad45daf1264044682d8f3639fe2c22ffa0699234
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-19 18:39:34 +0100

    refactor CommitmentBaseModel and CommitmentStoredClass to streamline properties and improve clarity

--- src/classes/CommitmentBaseModel.ts
+++ src/classes/CommitmentBaseModel.ts
@@ -1,9 +1,7 @@
-import { GraphicItemType, OnRenderGraphicItemProps, OnRunProps } from "@drincs/nqtr/dist/override";
+import { GraphicItemType, OnRenderGraphicItemProps } from "@drincs/nqtr/dist/override";
 import { CharacterInterface, getFlag, StoredClassModel } from "@drincs/pixi-vn";
 import { CommitmentProps } from "../interface";
 import { timeTracker } from "../managers";
-import { ExecutionType } from "../types";
-import { OnRunCommitmentEvent } from "../types/OnRunCommitmentEvent";
 import RoomBaseModel from "./navigation/RoomBaseModel";
 
 const COMMITMENT_CATEGORY = "__nqtr-commitment__"
@@ -49,76 +47,12 @@ export default class CommitmentBaseModel<TCharacter extends CharacterInterface =
         this.defaultPriority = props.priority || 0
     }
 
-    private _characters: TCharacter[]
-    /**
-     * The character or characters that are in the commitment and so in the room.
-     */
-    get characters(): TCharacter[] {
-        return this._characters
-    }
-
-    private _room: TRoom
-    /**
-     * The room where the commitment is.
-     */
-    get room(): TRoom {
-        return this._room
-    }
-
     private _name: string
     /**
      * The name
      */
     get name(): string {
         return this._name
-    }
-
-    private defaultFromHour?: number
-    /**
-     * The hour when the commitment starts. If the commitment is not started yet, it will be hidden.
-     * If you set undefined, it will return the initial value of fromHour.
-     */
-    get fromHour(): number {
-        return this.getStorageProperty<number>("fromHour") || this.defaultFromHour || timeTracker.minDayHours
-    }
-    set fromHour(value: number | undefined) {
-        this.setStorageProperty("fromHour", value)
-    }
-
-    private defaultToHour?: number
-    /**
-     * The hour when the commitment ends. If the commitment is ended yet, it will be hidden.
-     * If you set undefined, it will return the initial value of toHour.
-     */
-    get toHour(): number {
-        return this.getStorageProperty<number>("toHour") || this.defaultToHour || (timeTracker.maxDayHours + 1)
-    }
-    set toHour(value: number | undefined) {
-        this.setStorageProperty("toHour", value)
-    }
-
-    private defaultFromDay?: number
-    /**
-     * The day when the commitment starts. If the commitment is not started yet, it will be hidden.
-     * If you set undefined, it will return the initial value of fromDay.
-     */
-    get fromDay(): number | undefined {
-        return this.getStorageProperty<number>("fromDay") || this.defaultFromDay
-    }
-    set fromDay(value: number | undefined) {
-        this.setStorageProperty("fromDay", value)
-    }
-
-    private defaultToDay?: number
-    /**
-     * The day when the commitment ends. If the commitment is ended yet, it will be deleted or hidden.
-     * If you set undefined, it will return the initial value of toDay.
-     */
-    get toDay(): number | undefined {
-        return this.getStorageProperty<number>("toDay") || this.defaultToDay
-    }
-    set toDay(value: number | undefined) {
-        this.setStorageProperty("toDay", value)
     }
 
     private _renderImage?: GraphicItemType | ((commitment: CommitmentBaseModel<TCharacter, TRoom>, props: OnRenderGraphicItemProps) => GraphicItemType)
@@ -138,15 +72,6 @@ export default class CommitmentBaseModel<TCharacter extends CharacterInterface =
         return (props: OnRenderGraphicItemProps) => render
     }
 
-    private _executionType: ExecutionType
-    /**
-     * Execution type. If is "automatic" the onRun() runned automatically when the palayer is in the room. If is "interaction" the player must interact with the character to run the onRun() function.
-     * If you set "automatic" remember to remove the commitment when it is no longer needed, because otherwise it repeats itself every time.
-     */
-    get executionType(): ExecutionType {
-        return this._executionType
-    }
-
     private _renderIcon?: GraphicItemType | ((commitment: CommitmentBaseModel, props: OnRenderGraphicItemProps) => GraphicItemType)
     /**
      * The function for rendering the icon of the commitment.
@@ -162,18 +87,6 @@ export default class CommitmentBaseModel<TCharacter extends CharacterInterface =
             }
         }
         return (props: OnRenderGraphicItemProps) => render
-    }
-
-    private _onRun?: OnRunCommitmentEvent<CommitmentBaseModel>
-    /**
-     * Is a function that is called when the player interacts with the character.
-     */
-    get run(): undefined | ((props: OnRunProps) => void) {
-        let onRun = this._onRun
-        if (!onRun) {
-            return
-        }
-        return (props) => onRun(this, props)
     }
 
     private defaultDisabled: boolean | string
@@ -213,28 +126,5 @@ export default class CommitmentBaseModel<TCharacter extends CharacterInterface =
     }
     set hidden(value: boolean | string) {
         this.setStorageProperty("hidden", value)
-    }
-
-    private defaultPriority: number
-    /**
-     * The priority. The higher the number, the higher the priority.
-     * To ensure that a character is not in 2 places at the same time, if there are 2 or more valid commits at the same time and with the same character, the one with the highest priority will be chosen.
-     */
-    get priority(): number {
-        return this.getStorageProperty<number>("priority") || this.defaultPriority || 0
-    }
-    set priority(value: number) {
-        this.setStorageProperty("priority", value)
-    }
-
-    /**
-     * Whether the commitment is expired.
-     * @returns Whether the commitment is expired.
-     */
-    isExpired(): boolean {
-        if (this.toDay && this.toDay <= timeTracker.currentDay) {
-            return true
-        }
-        return false
     }
 }
