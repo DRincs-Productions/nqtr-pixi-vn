3dc863454b34e40b6bf4e0748b60ecdcea09103b
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-23 22:22:29 +0100

    style: standardize import formatting in index.ts

--- src/override/index.ts
+++ src/override/index.ts
@@ -1,14 +1,12 @@
-export { default as ActivityInterface } from './ActivityInterface';
-export { default as CommitmentInterface } from './CommitmentInterface';
-export { default as GraphicItemInterface } from './GraphicItemInterface';
-export { default as GraphicItemType } from './GraphicItemType';
-export { default as LocationInterface } from './LocationInterface';
-export { default as MapInterface } from './MapInterface';
-export { default as NeighboringMapsInterface } from './NeighboringMapsInterface';
-export { default as OnRenderGraphicItemProps } from './OnRenderGraphicItemProps';
-export { default as OnRunProps } from './OnRunProps';
-export { default as OnStartEndStageQuest } from './OnStartEndStageQuest';
-export { default as QuestInterface } from './QuestInterface';
-export { default as RoomInterface } from './RoomInterface';
-export { default as StageInterface } from './StageInterface';
-
+export { default as ActivityInterface } from "./ActivityInterface";
+export { default as CommitmentInterface } from "./CommitmentInterface";
+export { default as GraphicItemInterface } from "./GraphicItemInterface";
+export { default as GraphicItemType } from "./GraphicItemType";
+export { default as LocationInterface } from "./LocationInterface";
+export { default as MapInterface } from "./MapInterface";
+export { default as OnRenderGraphicItemProps } from "./OnRenderGraphicItemProps";
+export { default as OnRunProps } from "./OnRunProps";
+export { default as OnStartEndStageQuest } from "./OnStartEndStageQuest";
+export { default as QuestInterface } from "./QuestInterface";
+export { default as RoomInterface } from "./RoomInterface";
+export { default as StageInterface } from "./StageInterface";
