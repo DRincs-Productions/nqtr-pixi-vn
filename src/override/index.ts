573ec9bfb7c414636d7a47d645de36c8275e1a53
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-23 23:46:50 +0100

    refactor: remove GraphicItemType and update index exports accordingly

--- src/override/index.ts
+++ src/override/index.ts
@@ -1,7 +1,6 @@
 export { default as ActivityInterface } from "./ActivityInterface";
 export { default as CommitmentInterface } from "./CommitmentInterface";
 export { default as GraphicItemInterface } from "./GraphicItemInterface";
-export { default as GraphicItemType } from "./GraphicItemType";
 export { default as LocationInterface } from "./LocationInterface";
 export { default as MapInterface } from "./MapInterface";
 export { default as OnRenderGraphicItemProps } from "./OnRenderGraphicItemProps";
