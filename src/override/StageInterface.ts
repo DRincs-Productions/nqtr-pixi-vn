b875ff42aab8cf4cdeee70fa2c2a0287891b9c07
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-23 15:28:26 +0100

    enhance StageInterface with detailed JSDoc comments for improved documentation and clarity

--- src/override/StageInterface.ts
+++ src/override/StageInterface.ts
@@ -1,2 +1,38 @@
+import StageFlags from "../interface/quest/StageFlags";
+
 export default interface StageInterface {
+	/**
+	 * The name of the stage.
+	 */
+	readonly name: string;
+
+	/**
+	 * The description of the stage.
+	 */
+	readonly description: string;
+
+	/**
+	 * The advice description of the stage.
+	 */
+	readonly adviceDescription: string;
+
+	/**
+	 * The image of the stage.
+	 */
+	readonly image?: string;
+
+	/**
+	 * The list of flags that the player must complete to finish the stage.
+	 */
+	readonly flags: StageFlags[];
+
+	/**
+	 * The list of flags required to start the stage.
+	 */
+	readonly flagsRequiredToStart: StageFlags[];
+
+	/**
+	 * The description of the request to start the stage.
+	 */
+	readonly requestDescriptionToStart: string;
 }
