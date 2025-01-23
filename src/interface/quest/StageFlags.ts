e90608d2ad7bc9fe423b2ce4caf295ae82dd16a7
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-23 17:11:14 +0100

    feat: add readonly id property to StageFlags interface for stage identification

--- src/interface/quest/StageFlags.ts
+++ src/interface/quest/StageFlags.ts
@@ -1,10 +1,14 @@
 export default interface StageFlags {
-    /**
-     * The flag for checking if the condition is met.
-     */
-    flag: string
-    /**
-     * The description of the flag.
-     */
-    description: string
+	/**
+	 * The id of the stage.
+	 */
+	readonly id: string;
+	/**
+	 * The flag for checking if the condition is met.
+	 */
+	flag: string;
+	/**
+	 * The description of the flag.
+	 */
+	description: string;
 }
