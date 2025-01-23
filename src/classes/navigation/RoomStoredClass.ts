baa127a05db9fd84702c47173eae298650a8e2f6
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-23 15:36:26 +0100

    feat: add routine getter to RoomStoredClass for retrieving current routines

--- src/classes/navigation/RoomStoredClass.ts
+++ src/classes/navigation/RoomStoredClass.ts
@@ -1,20 +1,24 @@
-import { ActivityInterface, LocationInterface, RoomInterface } from "../../interface"
-import NavigationAbstractClass from "./NavigationAbstractClass"
+import { getCurrentRoutine } from "../../functions";
+import { ActivityInterface, CommitmentInterface, LocationInterface, RoomInterface } from "../../interface";
+import NavigationAbstractClass from "./NavigationAbstractClass";
 
-const ROOM_CATEGORY = "__nqtr-room__"
+const ROOM_CATEGORY = "__nqtr-room__";
 export default class RoomStoredClass extends NavigationAbstractClass implements RoomInterface {
-    constructor(
-        id: string,
-        /**
-         * The location where the room is.
-         */
-        private readonly _location: LocationInterface,
-        activities: ActivityInterface[] = []
-    ) {
-        super(ROOM_CATEGORY, id, activities)
-    }
+	constructor(
+		id: string,
+		/**
+		 * The location where the room is.
+		 */
+		private readonly _location: LocationInterface,
+		activities: ActivityInterface[] = []
+	) {
+		super(ROOM_CATEGORY, id, activities);
+	}
+	get routine(): CommitmentInterface[] {
+		return getCurrentRoutine().filter((c) => c.room.id === this.id);
+	}
 
-    get location(): LocationInterface {
-        return this._location
-    }
+	get location(): LocationInterface {
+		return this._location;
+	}
 }
