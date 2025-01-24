fa06bb438eb95caefcb27b1f68d4ebe3e8454533
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-24 18:41:10 +0100

    refactor: remove OnRenderGraphicItemProps interface and update index exports accordingly

--- src/override/index.ts
+++ src/override/index.ts
@@ -2,7 +2,6 @@ export { default as ActivityInterface } from "./ActivityInterface";
 export { default as CommitmentInterface } from "./CommitmentInterface";
 export { default as LocationInterface } from "./LocationInterface";
 export { default as MapInterface } from "./MapInterface";
-export { default as OnRenderGraphicItemProps } from "./OnRenderGraphicItemProps";
 export { default as OnRunProps } from "./OnRunProps";
 export { default as OnStartEndStageQuest } from "./OnStartEndStageQuest";
 export { default as QuestInterface } from "./QuestInterface";
