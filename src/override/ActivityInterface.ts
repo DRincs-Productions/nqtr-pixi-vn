28ed0a3844f943d8310cf8a785dcc5d75c5a4124
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-24 18:56:52 +0100

    refactor: simplify ActivityInterface by removing getter/setter methods for properties

--- src/override/ActivityInterface.ts
+++ src/override/ActivityInterface.ts
@@ -1,26 +1,18 @@
 export default interface ActivityInterface {
-    /**
-     * The name of the activity.
-     */
-    name: string
-    /**
-     * Whether is disabled. If it is a string, it is a Pixi'VN flag name.
-     */
-    get disabled(): boolean
-    /**
-     * Whether is hidden. If it is a string, it is a Pixi'VN flag name.
-     */
-    set disabled(value: boolean | string)
-    /**
-     * Whether is hidden. If it is a string, it is a Pixi'VN flag name.
-     */
-    get hidden(): boolean
-    /**
-     * Whether is hidden. If it is a string, it is a Pixi'VN flag name.
-     */
-    set hidden(value: boolean | string)
-    /**
-     * The icon of the activity.
-     */
-    readonly icon: string | undefined
+	/**
+	 * The name of the activity.
+	 */
+	name: string;
+	/**
+	 * Whether is disabled. If it is a string, it is a Pixi'VN flag name.
+	 */
+	disabled: boolean;
+	/**
+	 * Whether is hidden. If it is a string, it is a Pixi'VN flag name.
+	 */
+	hidden: boolean;
+	/**
+	 * The icon of the activity.
+	 */
+	readonly icon: string | undefined;
 }
