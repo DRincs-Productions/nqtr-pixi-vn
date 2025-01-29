6bca377575147d3ea70219adf084dfa15a528045
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 10:16:51 +0100

    refactor: update exports in index.ts for consistency and clarity

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,12 +1,13 @@
-export { default as ActivityBaseModel } from './ActivityBaseModel';
-export { default as ActivityStoredClass } from './ActivityStoredClass';
-export { default as CommitmentBaseModel } from './CommitmentBaseModel';
-export { default as LocationBaseModel } from './navigation/LocationBaseModel';
-export { default as LocationStoredClass } from './navigation/LocationStoredClass';
-export { default as MapBaseModel } from './navigation/MapBaseModel';
-export { default as MapStoredClass } from './navigation/MapStoredClass';
-export { default as RoomBaseModel } from './navigation/RoomBaseModel';
-export { default as RoomStoredClass } from './navigation/RoomStoredClass';
-export { default as Quest } from './quest/QuestBaseModel';
-export { default as Stage } from './quest/StageBaseModel';
-
+export { default as ActivityBaseModel } from "./ActivityBaseModel";
+export { default as ActivityStoredClass } from "./ActivityStoredClass";
+export { default as CommitmentBaseModel } from "./CommitmentBaseModel";
+export { default as LocationBaseModel } from "./navigation/LocationBaseModel";
+export { default as LocationStoredClass } from "./navigation/LocationStoredClass";
+export { default as MapBaseModel } from "./navigation/MapBaseModel";
+export { default as MapStoredClass } from "./navigation/MapStoredClass";
+export { default as RoomBaseModel } from "./navigation/RoomBaseModel";
+export { default as RoomStoredClass } from "./navigation/RoomStoredClass";
+export { default as Quest } from "./quest/QuestBaseModel";
+export { default as QuestStoredClass } from "./quest/QuestStoredClass";
+export { default as Stage } from "./quest/StageBaseModel";
+export { default as StageStoredClass } from "./quest/StageStoredClass";
