2ef611bf3392fdba3ebcec08c316a523d414149d
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 19:32:27 +0100

    chore: bump version to 0.3.6 and improve type definitions in NavigationAbstractClass

--- src/classes/navigation/NavigationAbstractClass.ts
+++ src/classes/navigation/NavigationAbstractClass.ts
@@ -5,210 +5,218 @@ import NavigationAbstractInterface from "../../interface/navigation/NavigationAb
 import { timeTracker } from "../../managers";
 
 type ActiveScheduling = {
-    fromHour?: number
-    toHour?: number
-    fromDay?: number
-    toDay?: number
-}
+    fromHour?: number;
+    toHour?: number;
+    fromDay?: number;
+    toDay?: number;
+};
 type ExcludedScheduling = {
-    toDay?: number
-}
+    toDay?: number;
+};
 
 export default abstract class NavigationAbstractClass extends StoredClassModel implements NavigationAbstractInterface {
-    constructor(
-        categoryId: string, id: string,
-        private defaultActivities: ActivityInterface[] = []
-    ) {
-        super(categoryId, id)
+    constructor(categoryId: string, id: string, private defaultActivities: ActivityInterface[] = []) {
+        super(categoryId, id);
     }
     private get defaultActivitiesIds(): string[] {
-        return this.defaultActivities.map(activity => activity.id)
+        return this.defaultActivities.map((activity) => activity.id);
     }
     private get activeActivityScheduling(): { [activityId: string]: ActiveScheduling } {
-        return this.getStorageProperty<{ [activityId: string]: ActiveScheduling }>(`activeActivityScheduling`) || {}
+        return this.getStorageProperty<{ [activityId: string]: ActiveScheduling }>(`activeActivityScheduling`) || {};
     }
     private get excludedActivitiesScheduling(): { [activityId: string]: ExcludedScheduling } {
-        return this.getStorageProperty<{ [activityId: string]: ExcludedScheduling }>(`excludedActivitiesScheduling`) || {}
+        return (
+            this.getStorageProperty<{ [activityId: string]: ExcludedScheduling }>(`excludedActivitiesScheduling`) || {}
+        );
     }
     private removeActivityScheduling(activityId: string) {
-        let activeActivityScheduling = this.activeActivityScheduling
-        delete activeActivityScheduling[activityId]
-        this.setStorageProperty(`activeActivityScheduling`, activeActivityScheduling)
-        let excludedActivitiesScheduling = this.excludedActivitiesScheduling
-        delete excludedActivitiesScheduling[activityId]
-        this.setStorageProperty(`excludedActivitiesScheduling`, excludedActivitiesScheduling)
+        let activeActivityScheduling = this.activeActivityScheduling;
+        delete activeActivityScheduling[activityId];
+        this.setStorageProperty(`activeActivityScheduling`, activeActivityScheduling);
+        let excludedActivitiesScheduling = this.excludedActivitiesScheduling;
+        delete excludedActivitiesScheduling[activityId];
+        this.setStorageProperty(`excludedActivitiesScheduling`, excludedActivitiesScheduling);
     }
     private editActivityScheduling(activityId: string, scheduling: ActiveScheduling) {
-        this.removeActivityScheduling(activityId)
-        let activeActivityScheduling = this.activeActivityScheduling
-        activeActivityScheduling[activityId] = scheduling
-        this.setStorageProperty(`activeActivityScheduling`, activeActivityScheduling)
+        this.removeActivityScheduling(activityId);
+        let activeActivityScheduling = this.activeActivityScheduling;
+        activeActivityScheduling[activityId] = scheduling;
+        this.setStorageProperty(`activeActivityScheduling`, activeActivityScheduling);
     }
     private editExcludedActivityScheduling(activityId: string, scheduling: ExcludedScheduling) {
-        this.removeActivityScheduling(activityId)
-        let excludedActivitiesScheduling = this.excludedActivitiesScheduling
-        excludedActivitiesScheduling[activityId] = scheduling
-        this.setStorageProperty(`excludedActivitiesScheduling`, excludedActivitiesScheduling)
+        this.removeActivityScheduling(activityId);
+        let excludedActivitiesScheduling = this.excludedActivitiesScheduling;
+        excludedActivitiesScheduling[activityId] = scheduling;
+        this.setStorageProperty(`excludedActivitiesScheduling`, excludedActivitiesScheduling);
     }
     private get additionalActivitiesIds(): string[] {
-        return this.getStorageProperty<string[]>(`additionalActivitiesIds`) || []
+        return this.getStorageProperty<string[]>(`additionalActivitiesIds`) || [];
     }
     private get excludedActivitiesIds(): string[] {
-        return this.getStorageProperty<string[]>(`excludedActivitiesIds`) || []
+        return this.getStorageProperty<string[]>(`excludedActivitiesIds`) || [];
     }
-    addActivity(activity: ActivityInterface, options: {
-        /**
-         * the activity will be associated with this class only for the specified hours.
-         * If you set from 3 and to 5, the activity will be associated with this class only for hours 3, 4 and 5. soSo at 2 or 6 it will not be associated with this class.
-         */
-        hours?: {
-            from: number
-            to: number
-        }
-        /**
-         * the activity will be associated with this class from the specified day.
-         * If you set 3, the activity will be associated with this class from day 3. So at day 2 it will not be associated with this class.
-         */
-        fromDay?: number
-        /**
-         * the activity will be associated with this class to the specified day.
-         * If you set 3, the activity will be associated with this class until day 3. So at day 4 it will not be associated with this class.
-         */
-        toDay?: number
-    } = {}) {
-        const { hours, fromDay, toDay } = options
-        let scheduling: ActiveScheduling = {}
+    addActivity(
+        activity: ActivityInterface,
+        options: {
+            /**
+             * the activity will be associated with this class only for the specified hours.
+             * If you set from 3 and to 5, the activity will be associated with this class only for hours 3, 4 and 5. soSo at 2 or 6 it will not be associated with this class.
+             */
+            hours?: {
+                from: number;
+                to: number;
+            };
+            /**
+             * the activity will be associated with this class from the specified day.
+             * If you set 3, the activity will be associated with this class from day 3. So at day 2 it will not be associated with this class.
+             */
+            fromDay?: number;
+            /**
+             * the activity will be associated with this class to the specified day.
+             * If you set 3, the activity will be associated with this class until day 3. So at day 4 it will not be associated with this class.
+             */
+            toDay?: number;
+        } = {}
+    ) {
+        const { hours, fromDay, toDay } = options;
+        let scheduling: ActiveScheduling = {};
         if (hours) {
             if (hours.from >= hours.to) {
-                throw new Error(`[NQTR] The from hour must be less than the to hour.`)
+                throw new Error(`[NQTR] The from hour must be less than the to hour.`);
             }
-            scheduling.fromHour = hours.from
+            scheduling.fromHour = hours.from;
         }
         if (fromDay === 0) {
-            console.warn(`[NQTR] The from day must be greater than 0, so it will be ignored.`)
+            console.warn(`[NQTR] The from day must be greater than 0, so it will be ignored.`);
         }
         if (toDay === 0) {
-            console.warn(`[NQTR] The to day must be greater than 0, so it will be ignored.`)
+            console.warn(`[NQTR] The to day must be greater than 0, so it will be ignored.`);
         }
         if (fromDay && toDay && fromDay >= toDay) {
-            throw new Error(`[NQTR] The from day must be less than the to day.`)
+            throw new Error(`[NQTR] The from day must be less than the to day.`);
         }
         if (fromDay) {
-            scheduling.fromDay = fromDay
+            scheduling.fromDay = fromDay;
         }
         if (toDay) {
-            scheduling.toDay = toDay
+            scheduling.toDay = toDay;
         }
 
         if (this.defaultActivitiesIds.includes(activity.id)) {
-            console.warn(`[NQTR] Activity with id ${activity.id} already exists, so it will be ignored.`)
-            return
+            console.warn(`[NQTR] Activity with id ${activity.id} already exists, so it will be ignored.`);
+            return;
         }
-        let additionalActivitiesIds = this.additionalActivitiesIds
+        let additionalActivitiesIds = this.additionalActivitiesIds;
         if (additionalActivitiesIds.includes(activity.id)) {
             if (Object.keys(scheduling).length) {
-                this.editActivityScheduling(activity.id, scheduling)
-                return
+                this.editActivityScheduling(activity.id, scheduling);
+                return;
+            } else if (this.excludedActivitiesIds.includes(activity.id)) {
+                this.removeActivityScheduling(activity.id);
+                console.log(
+                    `[NQTR] Activity with id ${activity.id} was excluded, so it will be associated with this class again.`
+                );
+                return;
             }
-            else if (this.excludedActivitiesIds.includes(activity.id)) {
-                this.removeActivityScheduling(activity.id)
-                console.log(`[NQTR] Activity with id ${activity.id} was excluded, so it will be associated with this class again.`)
-                return
-            }
-            console.warn(`[NQTR] Activity with id ${activity.id} already exists, so it will be ignored.`)
-            return
+            console.warn(`[NQTR] Activity with id ${activity.id} already exists, so it will be ignored.`);
+            return;
         }
-        additionalActivitiesIds.push(activity.id)
-        this.setStorageProperty(`additionalActivitiesIds`, additionalActivitiesIds)
+        additionalActivitiesIds.push(activity.id);
+        this.setStorageProperty(`additionalActivitiesIds`, additionalActivitiesIds);
         if (Object.keys(scheduling).length) {
-            this.editActivityScheduling(activity.id, scheduling)
+            this.editActivityScheduling(activity.id, scheduling);
         }
     }
-    revoveActivity(activity: ActivityInterface, options: {
-        /**
-         * The activity will be excluded from this class only for the specified days.
-         * If to 3, the activity will be excluded from this class only for days 1, 2 and 3. So at day 4 it will be associated with this class.
-         */
-        toDay?: number
-    } = {}) {
-        const { toDay } = options
-        let scheduling: ExcludedScheduling = {}
+    removeActivity(
+        activity: ActivityInterface,
+        options: {
+            /**
+             * The activity will be excluded from this class only for the specified days.
+             * If to 3, the activity will be excluded from this class only for days 1, 2 and 3. So at day 4 it will be associated with this class.
+             */
+            toDay?: number;
+        } = {}
+    ) {
+        const { toDay } = options;
+        let scheduling: ExcludedScheduling = {};
         if (toDay === 0) {
-            console.warn(`[NQTR] The to day must be greater than 0, so it will be ignored.`)
+            console.warn(`[NQTR] The to day must be greater than 0, so it will be ignored.`);
         }
         if (toDay) {
-            scheduling.toDay = toDay
+            scheduling.toDay = toDay;
         }
 
-        let additionalActivitiesIds = this.additionalActivitiesIds
+        let additionalActivitiesIds = this.additionalActivitiesIds;
         if (additionalActivitiesIds.includes(activity.id)) {
             if (Object.keys(scheduling).length) {
-                this.editExcludedActivityScheduling(activity.id, scheduling)
-                return
+                this.editExcludedActivityScheduling(activity.id, scheduling);
+                return;
             }
-            additionalActivitiesIds = additionalActivitiesIds.filter(id => id !== activity.id)
-            this.setStorageProperty(`additionalActivitiesIds`, additionalActivitiesIds)
-        }
-        else if (this.defaultActivitiesIds.includes(activity.id)) {
+            additionalActivitiesIds = additionalActivitiesIds.filter((id) => id !== activity.id);
+            this.setStorageProperty(`additionalActivitiesIds`, additionalActivitiesIds);
+        } else if (this.defaultActivitiesIds.includes(activity.id)) {
             if (Object.keys(scheduling).length) {
-                this.editExcludedActivityScheduling(activity.id, scheduling)
-                return
+                this.editExcludedActivityScheduling(activity.id, scheduling);
+                return;
             }
-            let excludedActivitiesIds = this.excludedActivitiesIds
-            excludedActivitiesIds.push(activity.id)
-            this.setStorageProperty(`excludedActivitiesIds`, excludedActivitiesIds)
-        }
-        else {
-            console.warn(`[NQTR] Activity with id ${activity.id} not found, so it will be ignored.`)
+            let excludedActivitiesIds = this.excludedActivitiesIds;
+            excludedActivitiesIds.push(activity.id);
+            this.setStorageProperty(`excludedActivitiesIds`, excludedActivitiesIds);
+        } else {
+            console.warn(`[NQTR] Activity with id ${activity.id} not found, so it will be ignored.`);
         }
     }
     clearExpiredActivities() {
-        let activeActivityScheduling = this.activeActivityScheduling
-        let excludedActivitiesScheduling = this.excludedActivitiesScheduling
-        let additionalActivitiesIds = this.additionalActivitiesIds
-        let excludedActivitiesIds = this.excludedActivitiesIds
+        let activeActivityScheduling = this.activeActivityScheduling;
+        let excludedActivitiesScheduling = this.excludedActivitiesScheduling;
+        let additionalActivitiesIds = this.additionalActivitiesIds;
+        let excludedActivitiesIds = this.excludedActivitiesIds;
 
-        additionalActivitiesIds.forEach(activityId => {
+        additionalActivitiesIds.forEach((activityId) => {
             if (activityId in activeActivityScheduling) {
-                let { toDay } = activeActivityScheduling[activityId]
+                let { toDay } = activeActivityScheduling[activityId];
                 if (toDay && toDay < timeTracker.currentDay) {
-                    this.removeActivityScheduling(activityId)
-                    additionalActivitiesIds = additionalActivitiesIds.filter(id => id !== activityId)
+                    this.removeActivityScheduling(activityId);
+                    additionalActivitiesIds = additionalActivitiesIds.filter((id) => id !== activityId);
                 }
             }
-        })
-        excludedActivitiesIds.forEach(activityId => {
+        });
+        excludedActivitiesIds.forEach((activityId) => {
             if (activityId in excludedActivitiesScheduling) {
-                let { toDay } = excludedActivitiesScheduling[activityId]
+                let { toDay } = excludedActivitiesScheduling[activityId];
                 if (toDay && toDay < timeTracker.currentDay) {
-                    this.removeActivityScheduling(activityId)
-                    excludedActivitiesIds = excludedActivitiesIds.filter(id => id !== activityId)
+                    this.removeActivityScheduling(activityId);
+                    excludedActivitiesIds = excludedActivitiesIds.filter((id) => id !== activityId);
                 }
             }
-        })
+        });
     }
 
     get activities(): ActivityInterface[] {
-        let res: ActivityInterface[] = []
+        let res: ActivityInterface[] = [];
         Object.entries(this.activeActivityScheduling).forEach(([activityId, scheduling]) => {
-            let activity = getActivityById(activityId)
-            const { fromDay = activity?.fromDay, fromHour = activity?.fromHour, toDay = activity?.toDay, toHour = activity?.toHour } = scheduling
-            if (activity &&
+            let activity = getActivityById(activityId);
+            const {
+                fromDay = activity?.fromDay,
+                fromHour = activity?.fromHour,
+                toDay = activity?.toDay,
+                toHour = activity?.toHour,
+            } = scheduling;
+            if (
+                activity &&
                 !(fromHour && toHour && !timeTracker.nowIsBetween(fromHour, toHour)) &&
                 !(fromDay && fromDay > timeTracker.currentDay) &&
                 !(toDay && toDay < timeTracker.currentDay)
             ) {
-                res.push(activity)
+                res.push(activity);
             }
-        })
+        });
         Object.entries(this.excludedActivitiesScheduling).forEach(([activityId, scheduling]) => {
-            let activity = getActivityById(activityId)
-            if (activity && activity.isActive &&
-                !(scheduling.toDay && scheduling.toDay >= timeTracker.currentDay)
-            ) {
-                res.push(activity)
+            let activity = getActivityById(activityId);
+            if (activity && activity.isActive && !(scheduling.toDay && scheduling.toDay >= timeTracker.currentDay)) {
+                res.push(activity);
             }
-        })
-        return res
+        });
+        return res;
     }
 }
--- src/interface/navigation/NavigationAbstractClass.ts
+++ src/interface/navigation/NavigationAbstractClass.ts
@@ -8,44 +8,50 @@ export default interface NavigationAbstractInterface extends StoredClassModel {
      * @param options
      * @returns
      */
-    addActivity(activity: ActivityInterface, options?: {
-        /**
-         * the activity will be associated with this class only for the specified hours.
-         * If you set from 3 and to 5, the activity will be associated with this class only for hours 3, 4 and 5. soSo at 2 or 6 it will not be associated with this class.
-         */
-        hours?: {
-            from: number
-            to: number
+    addActivity(
+        activity: ActivityInterface,
+        options?: {
+            /**
+             * the activity will be associated with this class only for the specified hours.
+             * If you set from 3 and to 5, the activity will be associated with this class only for hours 3, 4 and 5. soSo at 2 or 6 it will not be associated with this class.
+             */
+            hours?: {
+                from: number;
+                to: number;
+            };
+            /**
+             * the activity will be associated with this class from the specified day.
+             * If you set 3, the activity will be associated with this class from day 3. So at day 2 it will not be associated with this class.
+             */
+            fromDay?: number;
+            /**
+             * the activity will be associated with this class to the specified day.
+             * If you set 3, the activity will be associated with this class until day 3. So at day 4 it will not be associated with this class.
+             */
+            toDay?: number;
         }
-        /**
-         * the activity will be associated with this class from the specified day.
-         * If you set 3, the activity will be associated with this class from day 3. So at day 2 it will not be associated with this class.
-         */
-        fromDay?: number
-        /**
-         * the activity will be associated with this class to the specified day.
-         * If you set 3, the activity will be associated with this class until day 3. So at day 4 it will not be associated with this class.
-         */
-        toDay?: number
-    }): void
+    ): void;
     /**
      * Disconnects the activity from the class.
      * @param activity The activity to disconnect from the class.
-     * @param options 
+     * @param options
      */
-    revoveActivity(activity: ActivityInterface, options?: {
-        /**
-         * The activity will be excluded from this class only for the specified days.
-         * If to 3, the activity will be excluded from this class only for days 1, 2 and 3. So at day 4 it will be associated with this class.
-         */
-        toDay?: number
-    }): void
+    removeActivity(
+        activity: ActivityInterface,
+        options?: {
+            /**
+             * The activity will be excluded from this class only for the specified days.
+             * If to 3, the activity will be excluded from this class only for days 1, 2 and 3. So at day 4 it will be associated with this class.
+             */
+            toDay?: number;
+        }
+    ): void;
     /**
      * Removes the useless activities.
      */
-    clearExpiredActivities(): void
+    clearExpiredActivities(): void;
     /**
      * The activities associated with this class.
      */
-    activities: ActivityInterface[]
+    activities: ActivityInterface[];
 }
