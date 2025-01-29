883cd4abed17005af0ad32fefebe220189f3b2a1
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-01-29 15:05:25 +0100

    refactor: improve constructor parameter order and formatting for clarity

--- src/classes/CommitmentBaseModel.ts
+++ src/classes/CommitmentBaseModel.ts
@@ -20,99 +20,99 @@ import CommitmentStoredClass from "./CommitmentStoredClass";
  * ```
  */
 export default class CommitmentBaseModel extends CommitmentStoredClass {
-	/**
-	 * @param id The id of the commitment, it must be unique.
-	 * @param character The character or characters that are in the commitment and so in the room.
-	 * @param room The room where the commitment is.
-	 * @param props The properties of the commitment.
-	 */
-	constructor(
-		id: string,
-		character: CharacterInterface | CharacterInterface[] | undefined,
-		room: RoomInterface,
-		props: CommitmentProps
-	) {
-		super(
-			id,
-			character ? (Array.isArray(character) ? character : [character]) : [],
-			room,
-			props.executionType || "interaction",
-			props.priority || 0,
-			props.onRun,
-			{
-				fromHour: props.fromHour,
-				toHour: props.toHour,
-				fromDay: props.fromDay,
-				toDay: props.toDay,
-			}
-		);
-		this._name = props.name || "";
-		this._image = props.image;
-		this.defaultDisabled = props.disabled || false;
-		this.defaultHidden = props.hidden || false;
-		this._icon = props.icon;
-	}
+    /**
+     * @param id The id of the commitment, it must be unique.
+     * @param character The character or characters that are in the commitment and so in the room.
+     * @param room The room where the commitment is.
+     * @param props The properties of the commitment.
+     */
+    constructor(
+        id: string,
+        character: CharacterInterface | CharacterInterface[] | undefined,
+        room: RoomInterface,
+        props: CommitmentProps
+    ) {
+        super(
+            id,
+            character ? (Array.isArray(character) ? character : [character]) : [],
+            room,
+            props.onRun,
+            props.executionType || "interaction",
+            props.priority || 0,
+            {
+                fromHour: props.fromHour,
+                toHour: props.toHour,
+                fromDay: props.fromDay,
+                toDay: props.toDay,
+            }
+        );
+        this._name = props.name || "";
+        this._image = props.image;
+        this.defaultDisabled = props.disabled || false;
+        this.defaultHidden = props.hidden || false;
+        this._icon = props.icon;
+    }
 
-	private _name: string;
-	/**
-	 * The name of the commitment.
-	 */
-	get name(): string {
-		return this._name;
-	}
+    private _name: string;
+    /**
+     * The name of the commitment.
+     */
+    get name(): string {
+        return this._name;
+    }
 
-	private _image?: string;
-	/**
-	 * The image of the commitment.
-	 */
-	get image(): string | undefined {
-		return this._image;
-	}
+    private _image?: string;
+    /**
+     * The image of the commitment.
+     */
+    get image(): string | undefined {
+        return this._image;
+    }
 
-	private _icon?: string;
-	/**
-	 * The icon of the commitment.
-	 */
-	get icon(): string | undefined {
-		return this._icon;
-	}
+    private _icon?: string;
+    /**
+     * The icon of the commitment.
+     */
+    get icon(): string | undefined {
+        return this._icon;
+    }
 
-	private defaultDisabled: boolean | string;
-	/**
-	 * Whether is disabled. You can also pass a Pixi'VN flag name.
-	 */
-	get disabled(): boolean {
-		let value = this.getStorageProperty<boolean>("disabled") || this.defaultDisabled;
-		if (typeof value === "string") {
-			return getFlag(value);
-		}
-		return value;
-	}
-	set disabled(value: boolean | string) {
-		this.setStorageProperty("disabled", value);
-	}
+    private defaultDisabled: boolean | string;
+    /**
+     * Whether is disabled. You can also pass a Pixi'VN flag name.
+     */
+    get disabled(): boolean {
+        let value = this.getStorageProperty<boolean>("disabled") || this.defaultDisabled;
+        if (typeof value === "string") {
+            return getFlag(value);
+        }
+        return value;
+    }
+    set disabled(value: boolean | string) {
+        this.setStorageProperty("disabled", value);
+    }
 
-	private defaultHidden: boolean | string;
-	/**
-	 * Whether is hidden. You can also pass a Pixi'VN flag name.
-	 */
-	get hidden(): boolean {
-		if (this.fromDay && this.fromDay > timeTracker.currentDay) {
-			return true;
-		}
-		if (!timeTracker.nowIsBetween(this.fromHour, this.toHour)) {
-			return true;
-		}
-		if (!this.expired) {
-			return true;
-		}
-		let value = this.getStorageProperty<boolean>("hidden") || this.defaultHidden;
-		if (typeof value === "string") {
-			return getFlag(value);
-		}
-		return value;
-	}
-	set hidden(value: boolean | string) {
-		this.setStorageProperty("hidden", value);
-	}
+    private defaultHidden: boolean | string;
+    /**
+     * Whether is hidden. You can also pass a Pixi'VN flag name.
+     */
+    get hidden(): boolean {
+        if (this.fromDay && this.fromDay > timeTracker.currentDay) {
+            return true;
+        }
+        if (!timeTracker.nowIsBetween(this.fromHour, this.toHour)) {
+            return true;
+        }
+        if (!this.expired) {
+            return true;
+        }
+        let value = this.getStorageProperty<boolean>("hidden") || this.defaultHidden;
+        if (typeof value === "string") {
+            return getFlag(value);
+        }
+        return value;
+    }
+    set hidden(value: boolean | string) {
+        this.setStorageProperty("hidden", value);
+    }
 }
--- src/classes/CommitmentStoredClass.ts
+++ src/classes/CommitmentStoredClass.ts
@@ -1,49 +1,52 @@
-import { CharacterInterface } from "@drincs/pixi-vn"
-import { CommitmentInterface, RoomInterface } from "../interface"
-import { CommitmentBaseInternalInterface } from "../interface/CommitmentInterface"
-import { ExecutionType } from "../types"
-import { OnRunEvent } from "../types/OnRunEvent"
-import ActivityStoredClass from "./ActivityStoredClass"
+import { CharacterInterface } from "@drincs/pixi-vn";
+import { CommitmentInterface, RoomInterface } from "../interface";
+import { CommitmentBaseInternalInterface } from "../interface/CommitmentInterface";
+import { ExecutionType } from "../types";
+import { OnRunEvent } from "../types/OnRunEvent";
+import ActivityStoredClass from "./ActivityStoredClass";
 
-const COMMITMENT_CATEGORY = "__nqtr-commitment__"
-export default class CommitmentStoredClass extends ActivityStoredClass<CommitmentInterface> implements CommitmentBaseInternalInterface {
+const COMMITMENT_CATEGORY = "__nqtr-commitment__";
+export default class CommitmentStoredClass
+    extends ActivityStoredClass<CommitmentInterface>
+    implements CommitmentBaseInternalInterface
+{
     constructor(
         id: string,
         private readonly _characters: CharacterInterface[],
         private readonly _room: RoomInterface,
+        onRun: OnRunEvent<CommitmentInterface> | undefined,
         private readonly defaultExecutionType: ExecutionType,
         private readonly defaultPriority: number,
-        onRun: OnRunEvent<CommitmentInterface> | undefined,
         props: {
-            fromHour?: number
-            toHour?: number
-            fromDay?: number
-            toDay?: number
+            fromHour?: number;
+            toHour?: number;
+            fromDay?: number;
+            toDay?: number;
         }
     ) {
-        onRun = onRun || (() => { })
-        super(id, onRun, props, COMMITMENT_CATEGORY)
+        onRun = onRun || (() => {});
+        super(id, onRun, props, COMMITMENT_CATEGORY);
     }
 
     get characters(): CharacterInterface[] {
-        return this._characters
+        return this._characters;
     }
 
     get room(): RoomInterface {
-        return this._room
+        return this._room;
     }
 
     get executionType(): ExecutionType {
-        return this.getStorageProperty<ExecutionType>("executionType") || this.defaultExecutionType
+        return this.getStorageProperty<ExecutionType>("executionType") || this.defaultExecutionType;
     }
     set executionType(value: ExecutionType) {
-        this.setStorageProperty("executionType", value)
+        this.setStorageProperty("executionType", value);
     }
 
     get priority(): number {
-        return this.getStorageProperty<number>("priority") || this.defaultPriority || 0
+        return this.getStorageProperty<number>("priority") || this.defaultPriority || 0;
     }
     set priority(value: number) {
-        this.setStorageProperty("priority", value)
+        this.setStorageProperty("priority", value);
     }
 }
