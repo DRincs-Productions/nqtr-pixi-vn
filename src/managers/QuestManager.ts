0b0c78a5cac5a6ede100af1d288204fb25d1b514
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-02-08 23:38:42 +0100

    feat: add getters for quest states in QuestManager and improve documentation

--- src/managers/QuestManager.ts
+++ src/managers/QuestManager.ts
@@ -3,25 +3,47 @@ import { getQuestById, registeredQuests } from "../decorators/QuestDecorator";
 import { QuestInterface } from "../interface";
 
 export default class QuestManager {
+    /**
+     * The quests registered in the game.
+     */
     get quests(): QuestInterface[] {
         return Object.values(registeredQuests);
     }
+    /**
+     * The quests that are started, so they are in progress or completed or failed.
+     */
     get startedQuests(): QuestInterface[] {
         return this.quests.filter((quest) => quest.started);
     }
+    /**
+     * The quests that are in progress.
+     */
     get inProgressQuests(): QuestInterface[] {
         return this.quests.filter((quest) => quest.inProgress);
     }
+    /**
+     * The quests that are completed.
+     */
     get completedQuests(): QuestInterface[] {
         return this.quests.filter((quest) => quest.completed);
     }
+    /**
+     * The quests that are failed.
+     */
     get failedQuests(): QuestInterface[] {
         return this.quests.filter((quest) => quest.failed);
     }
+    /**
+     * The quests that are not started.
+     */
     get notStartedQuests(): QuestInterface[] {
         return this.quests.filter((quest) => !quest.started);
     }
-
+    /**
+     * Get the quest by the id.
+     * @param id The id of the quest.
+     * @returns The quest with the id.
+     */
     find(id: string): QuestInterface | undefined {
         return getQuestById(id);
     }
